207c7e2a2486091496c4183a8ba46a98889b20c3
Aman goyal <im.developer26@gmail.com>
2019-02-03 23:20:21 +0530

    Sets notification manager

--- app/src/main/java/com/example/amangoyal/funchat/FirebaseMessagingService.java
+++ app/src/main/java/com/example/amangoyal/funchat/FirebaseMessagingService.java
@@ -1,4 +1,31 @@
 package com.example.amangoyal.funchat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.RemoteMessage;
+
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        //Sets an id for the notification
+        int mNotificationId = 001;
+
+        //Gets an instance for the notification manager service
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Build the notification and issue it
+        notificationManager.notify(mNotificationId,mBuilder.build());
+    }
 }
