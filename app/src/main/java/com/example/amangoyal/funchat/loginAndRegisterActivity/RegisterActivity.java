576f4dcb57e3d5e798380b0027c45b4d9040cd0f
Aman goyal <im.developer26@gmail.com>
2019-02-05 23:27:44 +0530

    set Intent on click of notification

--- app/src/main/java/com/example/amangoyal/funchat/loginAndRegisterActivity/RegisterActivity.java
+++ app/src/main/java/com/example/amangoyal/funchat/loginAndRegisterActivity/RegisterActivity.java
@@ -21,7 +21,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
-
 import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
