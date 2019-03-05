af003c83d3ad08269d3615ff4e1fb8ba13ae1426
Aman goyal <im.developer26@gmail.com>
2019-03-05 23:57:52 +0530

    Friends Fragment

--- app/src/main/java/com/example/amangoyal/funchat/Fragments/FriendsFragment.java
+++ app/src/main/java/com/example/amangoyal/funchat/Fragments/FriendsFragment.java
@@ -22,9 +22,11 @@ import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.database.SnapshotParser;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,6 +64,19 @@ public class FriendsFragment extends Fragment {
 
     public void fetch(){
 
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot data : dataSnapshot.getChildren()){
+                    data.child(mCurrentUserId).getValue().toString();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })
     }
 
 
