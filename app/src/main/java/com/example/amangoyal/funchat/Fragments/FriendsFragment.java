893a69c62f229f616f07efc0f92f8c40014243c0
Aman goyal <im.developer26@gmail.com>
2019-03-07 23:07:02 +0530

    Friends Fragment

--- app/src/main/java/com/example/amangoyal/funchat/Fragments/FriendsFragment.java
+++ app/src/main/java/com/example/amangoyal/funchat/Fragments/FriendsFragment.java
@@ -104,6 +104,7 @@ public class FriendsFragment extends Fragment {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+
             }
         });
     }
